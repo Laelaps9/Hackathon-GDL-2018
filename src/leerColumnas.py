87bdd66c673ae2e0f561924ff8fdf8c3cb656ae4
Carlos Cabello <carloscabello364@gmail.com>
2018-03-18 03:36:24 -0600

    Conseguir Data

--- src/leerColumnas.py
+++ src/leerColumnas.py
@@ -4,10 +4,12 @@ import pandas as pd
 print ("Hola")
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.max_columns', 27)
-df = pd.ExcelFile('../Data/1996.xlsx').parse('BDRAM_1996')
 
-diasCO = []
-diasNO2 = []
+df = pd.ExcelFile('../Data/2007.xlsx').parse('BDRAM_2007')
+#df = pd.ExcelFile('../Data/2008.xlsx').parse('BDRAM_2008')
+#df = pd.ExcelFile('../Data/2009.xlsx').parse('BDRAM_09')
+#df = pd.ExcelFile('../Data/2010.xlsx').parse('BDRAM_2010')
+#df = pd.ExcelFile('../Data/2011.xlsx').parse('Data')
 
 df_CO = (df['PARAMETRO'] == 'CO')
 df_NO2 = (df['PARAMETRO'] == 'NO2')
@@ -46,59 +48,50 @@ df_H21 = (df['HORA21'])
 df_H22 = (df['HORA22'])
 df_H23 = (df['HORA23'])
 df_H24 = (df['HORA24'])
-df_CO2 = ((df_CO & df_AGU))
-#print(pd.Series(df_H1).iloc[0])
 
-dictCO = {}
-dias = []
-
-print(pd.Series(df_CO2).iloc[0])
-print(pd.Series(df_CO2).iloc[1])
 count = 0
 j = 0
 k = 0
 def promedioDias(dfa, dfp):
+    dias = []
     df_TEMP = (dfa & dfp)
     for i in range(len(df)):
         if(pd.Series(df_TEMP).iloc[i]) == 1:
             sumaHoras = 0
-            sumaHoras += pd.Series(df_H1).iloc[i]
-            sumaHoras += pd.Series(df_H2).iloc[i]
-            sumaHoras += pd.Series(df_H3).iloc[i]
-            sumaHoras += pd.Series(df_H4).iloc[i]
-            sumaHoras += pd.Series(df_H5).iloc[i]
-            sumaHoras += pd.Series(df_H6).iloc[i]
-            sumaHoras += pd.Series(df_H7).iloc[i]
-            sumaHoras += pd.Series(df_H8).iloc[i]
-            sumaHoras += pd.Series(df_H9).iloc[i]
-            sumaHoras += pd.Series(df_H10).iloc[i]
-            sumaHoras += pd.Series(df_H11).iloc[i]
-            sumaHoras += pd.Series(df_H12).iloc[i]
-            sumaHoras += pd.Series(df_H13).iloc[i]
-            sumaHoras += pd.Series(df_H14).iloc[i]
-            sumaHoras += pd.Series(df_H15).iloc[i]
-            sumaHoras += pd.Series(df_H16).iloc[i]
-            sumaHoras += pd.Series(df_H17).iloc[i]
-            sumaHoras += pd.Series(df_H18).iloc[i]
-            sumaHoras += pd.Series(df_H19).iloc[i]
-            sumaHoras += pd.Series(df_H20).iloc[i]
-            sumaHoras += pd.Series(df_H21).iloc[i]
-            sumaHoras += pd.Series(df_H22).iloc[i]
-            sumaHoras += pd.Series(df_H23).iloc[i]
-            sumaHoras += pd.Series(df_H24).iloc[i]
+            sumaHoras += float(pd.Series(df_H1).iloc[i])
+            sumaHoras += float(pd.Series(df_H2).iloc[i])
+            sumaHoras += float(pd.Series(df_H3).iloc[i])
+            sumaHoras += float(pd.Series(df_H4).iloc[i])
+            sumaHoras += float(pd.Series(df_H5).iloc[i])
+            sumaHoras += float(pd.Series(df_H6).iloc[i])
+            sumaHoras += float(pd.Series(df_H7).iloc[i])
+            sumaHoras += float(pd.Series(df_H8).iloc[i])
+            sumaHoras += float(pd.Series(df_H9).iloc[i])
+            sumaHoras += float(pd.Series(df_H10).iloc[i])
+            sumaHoras += float(pd.Series(df_H11).iloc[i])
+            sumaHoras += float(pd.Series(df_H12).iloc[i])
+            sumaHoras += float(pd.Series(df_H13).iloc[i])
+            sumaHoras += float(pd.Series(df_H14).iloc[i])
+            sumaHoras += float(pd.Series(df_H15).iloc[i])
+            sumaHoras += float(pd.Series(df_H16).iloc[i])
+            sumaHoras += float(pd.Series(df_H17).iloc[i])
+            sumaHoras += float(pd.Series(df_H18).iloc[i])
+            sumaHoras += float(pd.Series(df_H19).iloc[i])
+            sumaHoras += float(pd.Series(df_H20).iloc[i])
+            sumaHoras += float(pd.Series(df_H21).iloc[i])
+            sumaHoras += float(pd.Series(df_H22).iloc[i])
+            sumaHoras += float(pd.Series(df_H23).iloc[i])
+            sumaHoras += float(pd.Series(df_H24).iloc[i])
             dias.append(sumaHoras/24)
     dias.append(-1)
-promedioDias(df_AGU, df_CO)
-promedioDias(df_AGU, df_NO2)
-print(dias[504])
+    return dias
 
-'''def sumarHoras(dfa, dfp):
-    df_TEMP = (dfa & dfp)
-    tempArray = []
-    for i in range(len(df)):
-        if(pd.Series(df_TEMP).iloc) == 1:
-    '''
-
-#df_2 = df[df_CO]
-#df_final = df_2[df_AGU]
-#print(df_finalfinal)
+#promedioDias(df_AGU, df_CO)
+counter = 0
+anual = 0
+a = promedioDias(df_AGU, df_O3)
+while(a[counter] != -1):
+    print(a[counter])
+    anual += a[counter]
+    counter += 1
+#print(anual / (counter-1))
